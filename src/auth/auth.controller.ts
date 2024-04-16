a70a3c7e7fd25b36666a42c5bcce16cdf7d4a404
Yuliya Simonchyk <y.simonchyk@softteco.com>
2024-04-16 12:57:33 +0300

    ref:auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,139 +1,92 @@
 import {
-  Controller,
-  Post,
-  Body,
-  HttpCode,
-  HttpStatus,
-  Get,
-  BadRequestException,
-  UnauthorizedException,
-  Res,
-
+    Controller,
+    Post,
+    Body,
+    HttpCode,
+    HttpStatus,
+    Get,
+    Res,
+    InternalServerErrorException,
+    Logger,
 } from '@nestjs/common';
 
 import { Response } from 'express';
 import { InjectConfig } from 'nestjs-config';
 
-import {
-  ApiBadRequestResponse,
-  ApiOperation,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { Cookie, Public, UserAgent } from '../../libs/shared/src/decorators';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from './dto';
-import { Tokens } from './tokens';
 import { TokenEntity } from '../entities';
-
 const REFRESH_TOKEN = 'refreshtoken';
 
 @Public()
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    @InjectConfig() private readonly config,
-  ) {
-    this.config = config;
-  }
-
-  @HttpCode(HttpStatus.CREATED)
-  @Post('register')
-  @ApiResponse({ status: 201, description: 'User created.' })
-  @ApiOperation({ summary: 'Create a new user' })
-  @ApiResponse({ status: 201, description: 'User created successfully' })
-  @ApiBadRequestResponse({ description: 'Invalid input data' })
-  async register(@Body() userModel: RegisterDto) {
-    return this.authService.register(userModel);
-  }
-
-  @Post('login')
-  @ApiResponse({ status: 200, description: 'User logged in.' })
-  @ApiOperation({ summary: 'Login a user' })
-  @ApiBadRequestResponse({ description: 'Invalid input data' })
-  async login(
-    @Body() dto: LoginDto,
-    @Res() res: Response,
-    @UserAgent() agent: string,
-  ): Promise<{ accessToken: string }> {
-    const tokens = await this.authService.login(dto, agent);
-    if (!tokens) {
-      throw new BadRequestException('Invalid credentials');
-    }
-    this.setRefreshTokenToCookies(tokens, res);
-
-    return {
-      accessToken: tokens.accessToken,
-    };
-  }
-
-  @ApiResponse({ status: 200, description: 'User refreshed tokens.' })
-  @ApiOperation({ summary: 'Refresh tokens' })
-  @ApiBadRequestResponse({ description: 'Invalid refresh token' })
-  @Get('refresh-tokens')
-  async refreshTokens(
-    @Cookie(REFRESH_TOKEN) refreshToken: TokenEntity,
-    @Res() res: Response,
-    @UserAgent() agent: string,
-  ) {
-    if (!refreshToken) {
-      throw new UnauthorizedException();
+    private readonly logger = new Logger(AuthService.name);
+    constructor(
+        private readonly authService: AuthService,
+        @InjectConfig() private readonly config,
+    ) {
+        this.config = config;
     }
 
-    const tokens = await this.authService.refreshTokens(
-      refreshToken.token,
-      agent,
-    );
-
-    if (!tokens) {
-      throw new UnauthorizedException();
+    @HttpCode(HttpStatus.CREATED)
+    @Post('register')
+    @ApiResponse({ status: 201, description: 'User created.' })
+    @ApiOperation({ summary: 'Create a new user' })
+    @ApiResponse({ status: 201, description: 'User created successfully' })
+    @ApiBadRequestResponse({ description: 'Invalid input data' })
+    async register(@Body() userModel: RegisterDto) {
+        return this.authService.register(userModel);
     }
-    this.setRefreshTokenToCookies(tokens, res);
-  }
 
-  @Get('logout')
-  @ApiResponse({ status: 200, description: 'User logged out.' })
-  @ApiOperation({ summary: 'Logout a user' })
-  @ApiBadRequestResponse({ description: 'Invalid refresh token' })
-  async logout(
-    @Cookie(REFRESH_TOKEN) refreshToken: TokenEntity,
-    @Res() res: Response,
-  ) {
-    if (!refreshToken) {
-      res.sendStatus(HttpStatus.OK);
-      return;
+    @Post('login')
+    @ApiResponse({ status: 200, description: 'User logged in.' })
+    @ApiOperation({ summary: 'Login a user' })
+    @ApiBadRequestResponse({ description: 'Invalid input data' })
+    async login(
+        @Body() dto: LoginDto,
+        @Res() res: Response,
+        @UserAgent() agent: string,
+    ): Promise<{ accessToken: string }> {
+        try {
+            return await this.authService.login(dto, agent, res);
+        } catch (error) {
+            this.logger.error(`Failed to login user. Error: ${error.message}`, error.stack);
+            throw error;
+        }
     }
-    await this.authService.deleteRefreshToken(refreshToken.id);
-    res.cookie(REFRESH_TOKEN, '', {
-      httpOnly: true,
-      secure: true,
-      expires: new Date(),
-    });
-    res.sendStatus(HttpStatus.OK);
-  }
 
-  /**
-   * Sets the refresh token to cookies and returns the access token.
-   *
-   * @param {Tokens} tokens - The tokens object containing the refresh token.
-   * @param {Response} res - The response object to set the cookie on.
-   * @return {void}
-   */
-  private setRefreshTokenToCookies(tokens: Tokens, res: Response) {
-    if (!tokens) {
-      throw new UnauthorizedException();
+    @ApiResponse({ status: 200, description: 'User refreshed tokens.' })
+    @ApiOperation({ summary: 'Refresh tokens' })
+    @ApiBadRequestResponse({ description: 'Invalid refresh token' })
+    @Get('refresh-tokens')
+    async refreshTokens(
+        @Cookie(REFRESH_TOKEN) refreshToken: TokenEntity,
+        @Res() res: Response,
+        @UserAgent() agent: string,
+    ) {
+        try {
+            await this.authService.refreshTokens(refreshToken.token, agent, res);
+        } catch (error) {
+            this.logger.error(`Failed to refresh tokens. Error: ${error.message}`, error.stack);
+            throw new InternalServerErrorException();
+        }
     }
-    res.cookie(REFRESH_TOKEN, tokens.refreshToken, {
-      httpOnly: true,
-      sameSite: 'lax',
-      expires: new Date(tokens.refreshToken.exp),
-      secure: this.config.get('environment').NODE_ENV === 'PROD',
-      path: '/',
-    });
 
-    res.status(HttpStatus.CREATED).json({ accessToken: tokens.accessToken });
-  }
+    @Get('logout')
+    @ApiResponse({ status: 200, description: 'User logged out.' })
+    @ApiOperation({ summary: 'Logout a user' })
+    @ApiBadRequestResponse({ description: 'Invalid refresh token' })
+    async logout(@Cookie(REFRESH_TOKEN) refreshToken: TokenEntity, @Res() res: Response) {
+        try {
+            await this.authService.deleteRefreshToken(refreshToken, res);
+        } catch (error) {
+            this.logger.error(`Failed to logout user. Error: ${error.message}`, error.stack);
+            throw new InternalServerErrorException();
+        }
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,22 @@
-import { Injectable, BadRequestException, Body, Logger, UnauthorizedException } from '@nestjs/common';
+import { Injectable, BadRequestException, Body, Logger, UnauthorizedException, HttpStatus } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-
-import { UserService } from './../user';
-import { UserEntity } from './../entities';
-import { JwtPayload } from './interfaces';
-import { RegisterDto } from './dto/register.dto';
-import { LoginDto } from './dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Response } from 'express';
+import { InjectConfig } from 'nestjs-config';
 import { compareSync } from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
 import { addMonths } from 'date-fns';
-
-import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
+import { UserService } from './../user';
+import { UserEntity } from './../entities';
 import { TokenEntity } from '../entities/token.entity';
+import { JwtPayload } from './interfaces';
+import { LoginDto } from './dto';
+import { RegisterDto } from './dto/register.dto';
 import { Tokens } from './tokens';
+
+const REFRESH_TOKEN = 'refreshtoken';
 
 @Injectable()
 export class AuthService {
@@ -23,156 +26,176 @@ export class AuthService {
         private readonly jwtService: JwtService,
         @InjectRepository(TokenEntity)
         private readonly tokenRepository: Repository<TokenEntity>,
-    ) { }
+        @InjectConfig() private readonly config,
+    ) {
+        this.config = config;
+    }
 
-    /**
-     * A function that validates a user.
-     *
-     * @param {JwtPayload} payload - the payload containing user information
-     * @return {Promise<UserEntity | null>} the user entity if found, otherwise null
-     */
     async validateUser(payload: JwtPayload): Promise<UserEntity | null> {
         return await this.userService.findById(payload.id);
     }
 
-    /**
-     * Registers a new user.
-     *
-     * @param {RegisterDto} user - The user registration data.
-     * @return {Promise<string>} - A promise that resolves to the JWT token.
-     */
     async register(@Body() user: RegisterDto): Promise<string> {
-        const existingUser = await this.userService.findByEmail(user.email);
-        if (existingUser) {
-            this.logger.warn('User already exists');
-            throw new BadRequestException('User already exists');
+        try {
+            const existingUser = await this.userService.findByEmail(user.email);
+            if (existingUser) {
+                this.logger.warn('User already exists');
+                throw new BadRequestException('User already exists');
+            }
+            const createdUser = await this.userService.create(user);
+            const payload = { id: createdUser.id };
+            return await this.jwtService.signAsync(payload);
+        } catch (error) {
+            this.logger.error('Error during user registration', error.stack);
+            throw error;
         }
-        const createdUser = await this.userService.create(user);
-        const payload = { id: createdUser.id };
-        return this.jwtService.signAsync(payload);
     }
 
-    /**
-     * Authenticates a user by their email and password and generates tokens for them.
-     *
-     * @param {LoginDto} loginDto - An object containing the user's email and password.
-     * @param {string} agent - The user agent string.
-     * @return {Promise<any>} - A promise that resolves to the generated tokens.
-     */
-    async login({ email, password }: LoginDto, agent: string): Promise<any> {
-        const foundUser = await this.userService.findByCredentials(email, password);
+    async login({ email, password }: LoginDto, agent: string, res: Response): Promise<any> {
+        try {
+            const foundUser = await this.userService.findByCredentials(email, password);
 
-        if (!foundUser || !compareSync(password, foundUser.password)) {
-            this.logger.warn('Invalid credentials');
-            throw new BadRequestException('Invalid credentials');
+            if (!foundUser || !compareSync(password, foundUser.password)) {
+                this.logger.warn('Invalid credentials');
+                throw new BadRequestException('Invalid credentials');
+            }
+            const tokens = await this.generateTokens(foundUser, agent);
+            if (!tokens) {
+                throw new BadRequestException('Invalid credentials');
+            }
+            return this.setRefreshTokenToCookies(tokens, res);
+        } catch (error) {
+            this.logger.error('Error during authentication', error.stack);
+            throw error;
         }
-
-        return this.generateTokens(foundUser, agent);
     }
 
-    /**
-     * Async function to retrieve or create a refresh token for a user.
-     *
-     * @param {string} userId - The ID of the user
-     * @param {string} agent - The user agent
-     * @return {Promise<any>} A Promise that resolves with the retrieved or created token
-     */
     async getRefreshToken(userId: string, agent: string): Promise<any> {
-        const token = await this.tokenRepository
-            .createQueryBuilder('token')
-            .where('token.userId = :userId', { userId })
-            .andWhere('token.userAgent = :agent', { agent })
-            .getOne();
+        try {
+            const token = await this.tokenRepository
+                .createQueryBuilder('token')
+                .where('token.userId = :userId', { userId })
+                .andWhere('token.userAgent = :agent', { agent })
+                .getOne();
 
-        if (token) {
-            token.token = uuidv4();
-            token.exp = addMonths(new Date(), 1);
+            if (token) {
+                token.token = uuidv4();
+                token.exp = addMonths(new Date(), 1);
 
-            return await this.tokenRepository.save(token);
-        } else {
-            const newToken = new TokenEntity();
-            newToken.token = uuidv4();
-            newToken.exp = addMonths(new Date(), 1);
-            newToken.userId = userId;
-            newToken.userAgent = agent;
-            const createdToken = await this.tokenRepository.save(newToken);
+                return await this.tokenRepository.save(token);
+            } else {
+                const newToken = new TokenEntity();
+                newToken.token = uuidv4();
+                newToken.exp = addMonths(new Date(), 1);
+                newToken.userId = userId;
+                newToken.userAgent = agent;
+                const createdToken = await this.tokenRepository.save(newToken);
 
-            return createdToken;
+                return createdToken;
+            }
+        } catch (error) {
+            this.logger.error('Error during getRefreshToken', error.stack);
+            throw error;
         }
     }
-    /**
-     * Refreshes the access tokens for a user.
-     *
-     * @param {string} refreshToken - The refresh token to be used.
-     * @param {string} agent - The agent associated with the user.
-     * @return {Promise<any>} A promise that resolves to the generated tokens.
-     */
-    async refreshTokens(refreshToken: string, agent: string): Promise<any> {
-        // Find the token with the provided refresh token
-        const token = await this.tokenRepository.findOne({
-            where: { token: refreshToken },
-        });
 
-        // Check if the token doesn't exist or has expired
-        if (!token || token.exp < new Date()) {
-            // Delete the token
-            await this.tokenRepository.remove(token);
-            throw new UnauthorizedException('Invalid or expired refresh token');
+    async refreshTokens(refreshToken: string, agent: string, res: Response): Promise<any> {
+        try {
+            if (!refreshToken) {
+                throw new UnauthorizedException();
+            }
+
+            const token = await this.tokenRepository.findOne({
+                where: { token: refreshToken },
+            });
+
+            if (!token || token.exp < new Date()) {
+                await this.tokenRepository.remove(token);
+                throw new UnauthorizedException('Invalid or expired refresh token');
+            }
+
+            const user = await this.userService.findById(token.userId);
+
+            const tokens = await this.generateTokens(user, agent);
+            if (!tokens) {
+                throw new UnauthorizedException();
+            }
+            return this.setRefreshTokenToCookies(tokens, res);
+        } catch (error) {
+            this.logger.error('Error during refreshTokens', error.stack);
+            throw error;
         }
-
-        // Find the user associated with the token
-        const user = await this.userService.findById(token.userId);
-
-        // Generate new tokens for the user
-        return this.generateTokens(user, agent);
     }
-    /**
-     * Find a token by its value.
-     *
-     * @param {string} token - The token value to search for.
-     * @return {Promise<TokenEntity | null>} The found token entity or null if not found.
-     */
+
     async findToken(token: string): Promise<TokenEntity | null> {
-        return await this.tokenRepository.findOne({
-            where: {
-                token,
-            },
-        });
-    }
-    /**
-     * Generates access and refresh tokens for a given user and agent.
-     *
-     * @param {UserEntity} user - The user entity for whom the tokens are being generated.
-     * @param {string} agent - The agent for which the tokens are being generated.
-     * @return {Promise<Tokens>} A promise that resolves to an object containing the access and refresh tokens.
-     */
-    async generateTokens(user: UserEntity, agent: string): Promise<Tokens> {
-        const accessToken = this.jwtService.sign({
-            id: user.id,
-            email: user.email,
-            roles: user.role,
-        });
-        const refreshToken = await this.getRefreshToken(user.id, agent);
-        return { accessToken, refreshToken };
-    }
-
-    /**
-     * Deletes a refresh token from the database.
-     *
-     * @param {string} id - The ID of the refresh token to be deleted.
-     * @return {Promise<void>} - A promise that resolves when the token is successfully deleted.
-     */
-    async deleteRefreshToken(id: string) {
-        // Find the token by its ID
-        const token = await this.tokenRepository.createQueryBuilder('token').where('token.id = :id', { id }).getOne();
-
-        // Check if the token exists
-        if (!token) {
-            // Throw an error or handle the case where the token doesn't exist
-            throw new Error('Token not found');
+        try {
+            return await this.tokenRepository.findOne({
+                where: {
+                    token,
+                },
+            });
+        } catch (error) {
+            this.logger.error('Error during findToken', error.stack);
+            throw error;
         }
+    }
 
-        // Remove the token from the database
-        await this.tokenRepository.remove(token);
+    async generateTokens(user: UserEntity, agent: string): Promise<Tokens> {
+        try {
+            const accessToken = this.jwtService.sign({
+                id: user.id,
+                email: user.email,
+                roles: user.role,
+            });
+            const refreshToken = await this.getRefreshToken(user.id, agent);
+            return { accessToken, refreshToken };
+        } catch (error) {
+            this.logger.error('Error during generateTokens', error.stack);
+            throw error;
+        }
+    }
+
+    async deleteRefreshToken(refreshToken: TokenEntity, res: any) {
+        try {
+            if (!refreshToken) {
+                res.sendStatus(HttpStatus.OK);
+                return;
+            }
+
+            const token = await this.tokenRepository
+                .createQueryBuilder('token')
+                .where('token.id = :id', { id: refreshToken.id })
+                .getOne();
+
+            if (!token) {
+                throw new Error('Token not found');
+            }
+
+            await this.tokenRepository.remove(token);
+            res.cookie(REFRESH_TOKEN, '', {
+                httpOnly: true,
+                secure: true,
+                expires: new Date(),
+            });
+            res.sendStatus(HttpStatus.OK);
+        } catch (error) {
+            this.logger.error(`Error deleting refresh token: ${error.message}`, error.stack);
+            res.sendStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private setRefreshTokenToCookies(tokens: Tokens, res: Response) {
+        if (!tokens) {
+            throw new UnauthorizedException();
+        }
+        res.cookie(REFRESH_TOKEN, tokens.refreshToken, {
+            httpOnly: true,
+            sameSite: 'lax',
+            expires: new Date(tokens.refreshToken.exp),
+            secure: this.config.get('environment').NODE_ENV === 'PROD',
+            path: '/',
+        });
+
+        res.status(HttpStatus.CREATED).json({ accessToken: tokens.accessToken });
     }
 }
