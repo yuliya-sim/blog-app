d4b1d5306100bc8cfc4fd0ed4ef7618c5d02402a
Yuliya Simonchyk <y.simonchyk@softteco.com>
2024-04-21 20:03:00 +0300

    fix:time

--- client/src/api/helpers/calculate.ts
+++ client/src/api/helpers/calculate.ts
@@ -1,17 +1,8 @@
 export const calculateTimePassed = (givenDate: Date) => {
-    const dateComment = new Date(givenDate);
-    const now = new Date();
-    const delta = Math.abs(now - dateComment) / 1000;
+    const date = new Date(givenDate);
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${day}.${month}.${year}`;
 
-    const days = Math.floor(delta / 86400);
-    const hours = Math.floor(delta / 3600) % 24;
-    const minutes = Math.floor(delta / 60) % 60;
-
-    if (days > 0) {
-        return `${days} days ago`;
-    } else if (hours > 0) {
-        return `${hours} hours ago`;
-    } else {
-        return `${minutes} minutes ago`;
-    }
 }
