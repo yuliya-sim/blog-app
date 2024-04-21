2dbcebced5c11998c3c64eadc22a21ec0b82b453
Yuliya Simonchyk <y.simonchyk@softteco.com>
2024-04-21 13:49:33 +0300

    fix:login

--- client/src/api/helpers/axios/login.ts
+++ client/src/api/helpers/axios/login.ts
@@ -1,9 +1,7 @@
 
 import axios from 'axios';
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-import { useNavigate } from 'react-router-dom';
 export async function Login(formData: any) {
-    const navigate = useNavigate();
     try {
         const response = await axios.post(
             `${import.meta.env.VITE_BACKEND_URL}auth/login`,
@@ -12,7 +10,6 @@ export async function Login(formData: any) {
         const { accessToken } = response.data;
         localStorage.setItem('token', accessToken);
         window.location.href = '/';
-        navigate('/');
     } catch (error) {
         console.error('Sign in failed:', error);
     }
